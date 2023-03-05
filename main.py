7d87b6121c77e17b0966eb07caa7221f5e12334c
sreejith <squeeky.mouse@gmail.com>
2023-03-05 23:26:49 +0530

    store records in sqlite db Sec39

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import selectorlib
 # from send_email import send_email
 import os
 import smtplib, ssl
-# commit: send email Sec38
+import time
+import sqlite3
+# commit: store records in sqlite db Sec39
+
+# "INSERT INTO events VALUES ('Tigers','Tiger City','2088.10.14')"
+# "SELECT * FROM events WHERE date='2088.10.15'"
+# "DELETE FROM events WHERE band='Tigres'"
 
 URL='http://programmer100.pythonanywhere.com/tours/'
 # some servers dont allow scraping so supply headers if necessary!!!
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# Establish a connection
+dbconnection=sqlite3.connect('db/data.sqlite')
 
 def scrape(url):
     """Scrape the page source from the url"""
@@ -35,21 +44,33 @@ def send_email(message):
         server.sendmail(from_addr=username,to_addrs=receiver,msg=message)
 
 def store(extracted):
-    with open('data.txt','a') as file:
-        file.write(f'{extracted}\n')
+    row=extracted.split(',')
+    row=[item.strip() for item in row]
+    cursor=dbconnection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)",row)
+    dbconnection.commit()
 
 def read(extracted):
-    with open('data.txt','r') as file:
-        return(file.read())
+    # Feng Suave, Minimalia City, 5.5.2089
+    row=extracted.split(',')
+    row=[item.strip() for item in row]
+    band,city,date=row
+    cursor=dbconnection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
+                    (band,city,date))
+    rows=cursor.fetchall()
+    print(rows)
+    return(rows)
 
 if __name__=='__main__':
-    scraped=scrape(URL)
-    extracted=extract(scraped)
-    print(extracted)
-    content=read(extracted)
+    while True:
+        scraped=scrape(URL)
+        extracted=extract(scraped)
+        print(extracted)
 
-    if extracted!='No upcoming tours':
-        if extracted not in content: # dont send multiple mails for smae event
-            store(extracted) # store emailed events in file
-            send_email(message='Hey, new event was found')
-            
+        if extracted!='No upcoming tours':
+            row=read(extracted) # to check if its already present in db
+            if not row: # check for empty list!!! non-empty is True, empty is False!!!
+                store(extracted) 
+                send_email(message='Hey, new event was found')
+        time.sleep(2)
